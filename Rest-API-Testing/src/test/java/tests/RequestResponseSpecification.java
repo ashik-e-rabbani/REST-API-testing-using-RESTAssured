76ac3def42e244f4ed001c574a441d0eaaa64560
Ashik Rabbani <ashik.ashik.rabbni@gmail.com>
2023-05-18 00:26:05 +0600

    Create Request Response specifications
    Declare a set of request contents and common validations in response section, use it any where in the class

---
+++ Rest-API-Testing/src/test/java/tests/RequestResponseSpecification.java
@@ -0,0 +1,44 @@
+package tests;
+
+import io.restassured.RestAssured;
+import io.restassured.builder.ResponseSpecBuilder;
+import io.restassured.specification.RequestSpecification;
+import io.restassured.specification.ResponseSpecification;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+/*
+ These specifications allow you to define common configurations, headers, query parameters,
+ and assertions that can be shared across multiple API requests.
+ */
+public class RequestResponseSpecification {
+
+//    Declare Specifications here for global use
+    RequestSpecification reqSpec;
+    ResponseSpecification resSpec;
+
+    @BeforeClass
+    public void InitializeRequestResponse(){
+
+        reqSpec = RestAssured.given()
+                .baseUri("https://reqres.in/");
+        resSpec = RestAssured.expect()
+                .statusCode(201);
+
+
+    }
+@Test
+    public void doRegister(){
+        RestAssured.given()
+//              Provide the initialized request here
+                .spec(reqSpec)
+                .body("{\n" +
+                        "    \"name\": \"morpheus\",\n" +
+                        "    \"job\": \"leader\"\n" +
+                        "}")
+                .post("api/users")
+                .then()
+//              Provide the initialized response assertion here
+                .spec(resSpec);
+    }
+}
